acd7043d15c00901430ccce0588b0d0a30b48640
keybdwarrior <keybdwarrior@users.noreply.github.com>
2023-11-28 15:17:33 +0900

    Create study.c

---
+++ study.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+void input_num(void);
+int d1,d2;
+int get_max(int,int);
+void print_max();
+
+int main(void)
+{
+input_num(); // int
+int d1,d2;
+printf(">>1\n");
+scanf("%d",&d1);
+printf(">>2\n");
+scanf("%d",&d2);
+if(d1>d2)
+printf("%d\n",d1);
+else
+printf("%d\n",d2);
+}
+
+void input_num(void)
+printf(">>1\n");
+scanf("%d",&d1);
+printf(">>2\n");
+scanf("%d",&d2);
+
+int get_max(int,int)
+if(d1>d2)
+printf("%d\n",d1);  //return?
+else
+printf("%d\n",d2);
+
+void print_max()
+{
+printf("%d",x)
+}
